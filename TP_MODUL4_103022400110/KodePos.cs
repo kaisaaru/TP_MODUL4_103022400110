d263cfa28924acdd9316aa04ca566b6f417c4ce7
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-03-15 22:38:23 +0700

    second commit

---
+++ TP_MODUL4_103022400110/KodePos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class KodePos
+{
+    private Dictionary<string, int> kodePos = new Dictionary<string, int>()
+    {
+        {"Batununggal",40266},
+        {"Kujangsari",40287},
+        {"Mengger",40267},
+        {"Wates",40256},
+        {"Cijaura",40287},
+        {"Jatisari",40286},
+        {"Margasari",40286},
+        {"Sekejati",40286},
+        {"Kebonwaru",40272},
+        {"Maleer",40274}
+    };
+
+    public int getKodePos(string kelurahan)
+    {
+        return kodePos[kelurahan];
+    }
+}
--- TP_MODUL4_103022400110/Program.cs
+++ TP_MODUL4_103022400110/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kode = new KodePos();
+
+        Console.Write("Masukkan Kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        int kodepos = kode.getKodePos(kelurahan);
+
+        Console.WriteLine("Kode Pos: " + kodepos);
+    }
+}
